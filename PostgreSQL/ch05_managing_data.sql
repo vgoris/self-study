e585f6abbad6e616790f83e48926bdfb01e127cb
vgoris <74996157+vgoris@users.noreply.github.com>
2021-01-14 11:19:25 -0500

    Update ch05_managing_data.sql

--- PostgreSQL/ch05_managing_data.sql
+++ PostgreSQL/ch05_managing_data.sql
@@ -3,7 +3,6 @@
 /* Creating an index for an existing table 
    NULL LAST lists null values at the end 
 */
-
 CREATE INDEX products_product_id_idx
   ON manufacturing.products USING btree
   (product_id ASC NULL LAST)
@@ -13,7 +12,6 @@ CREATE INDEX products_product_id_idx
       that category_id and sets it to that value automatically
       if left blank in table 
  */
- 
 ALTER TABLE manufacturing.products
   ALTER COLUMN category_id SET DEFAULT 4;
   
@@ -23,7 +21,6 @@ ALTER TABLE manufacturing.products
    Protects against spelling errors and other 
    inconsistencies.
 */
-
 ALTER TABLE manufacturing.products
   ADD CONSTRAINT categories_market_check 
     CHECK (market = 'domestic' OR market = 'industrial');
@@ -35,8 +32,7 @@ ALTER TABLE manufacturing.products
       3. Add check constraint 'hire_date > '2020-01-01''  
  */
  
- /* 1. Add indexes */
- 
+ /* 1. Add indexes */ 
 CREATE INDEX employees_employee_id_idx
     ON human_resources.employees USING btree
     (employee_id ASC NULLS LAST);
@@ -48,12 +44,10 @@ CREATE INDEX employees_last_name_idx
 /* 2. Add constraint 
       Look for the training department's id first
 */
-
 SELECT * FROM human_resources.departments
   WHERE department_name = 'Training';
 
 /* The department's id is 800 */
-
 ALTER TABLE human_resources.employees
    ALTER COLUMN department_id SET DEFAULT 800;
    
@@ -65,7 +59,6 @@ ALTER TABLE human_resources.employees
       into the table doesn't get validated with this new check- only applies
       to new data. Employees hired before January 1st remain in the table
 */
-
 ALTER TABLE human_resources.employees
   ADD CONSTRAINT employees_hire_date_check 
   CHECK (hire_date > '2020-01-01')
