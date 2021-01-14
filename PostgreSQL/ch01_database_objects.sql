f7e2053ccfd5083d2836ee0c41c7153f970d49db
vgoris <74996157+vgoris@users.noreply.github.com>
2021-01-14 11:22:58 -0500

    Update ch01_database_objects.sql

--- PostgreSQL/ch01_database_objects.sql
+++ PostgreSQL/ch01_database_objects.sql
@@ -2,15 +2,22 @@
                 2. Store information about different kinds of berries
                 3. Table columns should include an ID number, berry name, and color */
 
-/* Create table with three columns */
+/* Create table with three columns
+     Convention to use lowercase letters for table and column names,
+     and to separate multiple words with an underscore (_)
+*/
 CREATE TABLE public.kinds_of_berries
 ( 
+  /* Variables that define the columns of the table
+     character(20) indicates sequence that stores up to 20 characters */
   "berry_id" integer,
   "berry_name" character(20),
   "berry_color" character(20)
 );
 
-/* Add values to rows */
+/* Add values to rows 
+     Enter text in Postgres in single quotation marks.
+*/
 INSERT INTO public."kinds_of_berries" (
   "berry_id", "berry_name", "berry_color")
   VALUES (1, 'blueberry', 'blue'), 
