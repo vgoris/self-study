185769b510b34d0a5b6fba5bbb25293718c93adb
vgoris <74996157+vgoris@users.noreply.github.com>
2021-01-14 11:17:57 -0500

    Update ch04_querying_data.sql

--- PostgreSQL/ch04_querying_data.sql
+++ PostgreSQL/ch04_querying_data.sql
@@ -1,11 +1,23 @@
-/* Practice: Retrieving information from a table */
+/* PRACTICE: Retrieving information from a table */
 
+/* SELECT keyword used to ask for specific columns of a table
+       * retrieves all columns, otherwise can specify which columns 
+       (separated by comma) and they will be returned in that order
+   FROM keyword specifies which table
+   WHERE keyword selects for certain rows against some criteria 
+       (example: WHERE category_id = 3)
+*/
 SELECT name, manufacturing_cost
 FROM manufacturing.products
 WHERE category_id = 3;
 
-/* Joining tables with existing relationship */
-
+/* Joining tables with existing relationship 
+       Use AS keyword to add an alias to a column name:
+              when column name is too long, 
+              or two specify which table the column comes from
+              if two columns from different tables have the same name
+       ON keyword specifies which column hold the related values 
+*/
 SELECT products.product_id,
        products.name AS product_name,
        products.manufacturing_cost,
