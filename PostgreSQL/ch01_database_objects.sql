e5df19f3230444ab5efed5539b89331188689f1b
vgoris <74996157+vgoris@users.noreply.github.com>
2021-01-14 11:07:14 -0500

    Update ch01_database_objects.sql

--- PostgreSQL/ch01_database_objects.sql
+++ PostgreSQL/ch01_database_objects.sql
@@ -1,6 +1,6 @@
-/* Example:   1. Create a table in an existing database using pgAdmin and add data
-              2. Store information about different kinds of berries
-              3. Table columns should include an ID number, berry name, and color */
+/* CHALLENGE:   1. Create a table in an existing database using pgAdmin and add data
+                2. Store information about different kinds of berries
+                3. Table columns should include an ID number, berry name, and color */
 
 /* Create table with three columns */
 CREATE TABLE public.kinds_of_berries
