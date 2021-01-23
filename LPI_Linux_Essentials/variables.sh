f3a216eb69f0016eadbf395b6e011377762e8541
vgoris <74996157+vgoris@users.noreply.github.com>
2021-01-23 15:57:22 -0500

    Create variables.sh

---
+++ LPI_Linux_Essentials/variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+# Purpose: practice variable declaration commands 
+# Modified date: 2020-01-23
+
+# Linux rules and conventions for naming and declaring variables:
+#   1. variable names must start with letter
+#   2. use underscores in place of spaces between words
+#   3. no punctuation marks
+#   4. start variable declaration with $
+#   5. variable with all upper-case letters is an environmental variable
+
+$age=40
